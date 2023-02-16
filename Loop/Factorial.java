e9713b1f92e1fc8ce1199d4fc9de105c453b2449
deepaktechnical007 <85184074+deepaktechnical007@users.noreply.github.com>
2023-02-17 00:52:56 +0530

    Factorial

---
+++ Loop/Factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String [] args ){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num =sc.nextInt();
+        int fact = 1;
+
+        for(int i = 1; i<=num; i++){
+
+            fact = fact*i;
+        }
+ System.out.println("Factorial of "+num+" is "+fact);
+
+    }
+
+}
