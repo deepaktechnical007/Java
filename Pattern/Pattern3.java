d930ae9c4242f5572121a591c0d4c57c8bffb728
deepaktechnical007 <85184074+deepaktechnical007@users.noreply.github.com>
2023-02-26 11:12:04 +0530

    Star pattern

---
+++ Pattern/Pattern3.java
@@ -0,0 +1,19 @@
+public class Pattern3 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 4; i++) {
+
+            for (int j = 4; j >= i; j--) {
+
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
