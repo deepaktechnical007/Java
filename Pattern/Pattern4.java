555aa74673577f33e5da763c3301d8c08dd5ecde
deepaktechnical007 <85184074+deepaktechnical007@users.noreply.github.com>
2023-02-27 01:51:34 +0530

    Right down Mirror Star Pattern

---
+++ Pattern/Pattern4.java
@@ -0,0 +1,19 @@
+public class Pattern4 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 4; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(" ");
+            }
+
+            for (int k = 4; k >= i; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
