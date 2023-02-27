135a4f535304a6d72298b99db29cb7357782b6a8
deepaktechnical007 <85184074+deepaktechnical007@users.noreply.github.com>
2023-02-28 00:21:50 +0530

    Different method Pyramid Program

---
+++ Pattern/Pattern8.java
@@ -0,0 +1,19 @@
+public class Pattern8 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 4; i++) {
+
+            for (int j = 4; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            for (int l = 2; l <= i; l++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
