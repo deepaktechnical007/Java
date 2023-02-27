8c3375f2fab17ece45eb25498ffbea7cdc18e1ba
deepaktechnical007 <85184074+deepaktechnical007@users.noreply.github.com>
2023-02-28 00:28:58 +0530

    Different method Reversed Pyramid Star Pattern

---
+++ Pattern/Pattern12.java
@@ -0,0 +1,16 @@
+public class Pattern12 {
+
+    public static void main(String[] args) {
+
+        for (int i = 4; i >= 1; i--) {
+
+            for (int j = 4; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k < (i * 2); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
