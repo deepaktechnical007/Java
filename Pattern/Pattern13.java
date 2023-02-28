94a8059320cb455bfd21b9f1e017c1797d618666
deepaktechnical007 <85184074+deepaktechnical007@users.noreply.github.com>
2023-03-01 00:34:54 +0530

    Diagonal star pattern

---
+++ Pattern/Pattern13.java
@@ -0,0 +1,18 @@
+public class Pattern13 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                if (i >= 2 && j <= i - 1) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
