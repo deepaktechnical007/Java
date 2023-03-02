7dd386f38c926ef929e47caa35f541239f6df84a
deepaktechnical007 <85184074+deepaktechnical007@users.noreply.github.com>
2023-03-03 01:10:43 +0530

    Left Down Number Triangle

---
+++ Pattern/Pattern24.java
@@ -0,0 +1,13 @@
+public class Pattern24 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 5; j >= i; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
