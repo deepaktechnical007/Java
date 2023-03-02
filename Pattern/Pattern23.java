0dd29a66e89c7e892d22384030d245e88170e8a0
deepaktechnical007 <85184074+deepaktechnical007@users.noreply.github.com>
2023-03-03 01:08:58 +0530

    Left triangle number pattern

---
+++ Pattern/Pattern23.java
@@ -0,0 +1,17 @@
+public class Pattern23 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(j + " ");
+            }
+            for (int k = i - 1; k >= 1; k--) {
+                System.out.print(k + " ");
+            }
+            System.out.println();
+        }
+    }
+}
