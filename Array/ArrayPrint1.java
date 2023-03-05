cfc078e028ef27b7e669a7786acbabff7d4f7494
deepaktechnical007 <85184074+deepaktechnical007@users.noreply.github.com>
2023-03-05 14:35:53 +0530

    Print Array

---
+++ Array/ArrayPrint1.java
@@ -0,0 +1,12 @@
+public class ArrayPrint1 {
+
+    public static void main(String[] args) {
+
+        int[] a = { 10, 20, 30, 40 };
+
+        for (int i : a) {
+
+            System.out.print(i + " ");
+        }
+    }
+}
