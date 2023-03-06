ed525fe0dc9b27e7c1067a26f6e7e37f1605738f
deepaktechnical007 <85184074+deepaktechnical007@users.noreply.github.com>
2023-03-06 11:48:26 +0530

    Three dimention array

---
+++ Array/ThreeDimArray.java
@@ -0,0 +1,19 @@
+public class ThreeDimArray {
+
+    public static void main(String[] args) {
+
+        int[][][] a = { { { 10, 20 }, { 30, 40, 50, 60 }, { 70, 80, 90 } } };
+
+        for (int i = 0; i < a.length; i++) {
+
+            for (int j = 0; j < a[i].length; j++) {
+
+                for (int k = 0; k < a[i][j].length; k++) {
+
+                    System.out.print(a[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
