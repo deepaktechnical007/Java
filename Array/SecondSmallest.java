ae14a0f4b78505c18a43aa5b5cf924d466c93163
deepaktechnical007 <85184074+deepaktechnical007@users.noreply.github.com>
2023-03-12 16:10:43 +0530

    Second Smallest number

---
+++ Array/SecondSmallest.java
@@ -0,0 +1,30 @@
+class SecondSmallest {
+
+    public static void main(String[] args) {
+
+        int[] a = { 5, 7, 2, 11, 9, 1, 3, 6 };
+        int temp;
+
+        for (int i = 0; i < a.length; i++) {
+
+            for (int j = i + 1; j < a.length; j++) {
+
+                if (a[i] > a[j]) {
+
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+
+        }
+        for (int k = 0; k < a.length; k++) {
+
+            System.out.print(a[k] + " ");
+
+        }
+
+        System.out.println("Second smallest number is " + a[1]);
+
+    }
+}
