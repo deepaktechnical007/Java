e37d67a42234f2691e6bba7c9fd618b8e6694ee4
deepaktechnical007 <85184074+deepaktechnical007@users.noreply.github.com>
2023-03-22 23:26:50 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+
+    public void eat() {
+        System.out.println("i am eating");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1");
+        Animal buzo = new Animal();
+        buzo.eat();
+    }
+}
