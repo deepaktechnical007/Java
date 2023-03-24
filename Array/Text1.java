90b2a980a56f43f240dee64e4773fe46986a04a8
deepaktechnical007 <85184074+deepaktechnical007@users.noreply.github.com>
2023-03-24 19:44:06 +0530

    No argument constructor

---
+++ Array/Text1.java
@@ -0,0 +1,11 @@
+public class Text1 {
+
+    Text1() {
+        System.out.println("no argument constructor");
+    }
+
+    public static void main(String[] args) {
+
+        Text1 t = new Text1();
+    }
+}
