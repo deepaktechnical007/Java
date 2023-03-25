5f9457c5394e4c60eeca419bb16adde28ee1ffe4
deepaktechnical007 <85184074+deepaktechnical007@users.noreply.github.com>
2023-03-25 12:55:12 +0530

    Parametrized constructor

---
+++ Array/Text2.java
@@ -0,0 +1,11 @@
+public class Text2 {
+
+    Text2(int i) {
+        System.out.println("parametrized constructor");
+    }
+
+    public static void main(String[] args) {
+
+        Text2 t = new Text2(10);
+    }
+}
