baac3b41548fd124bcab29a9684e7fb307e0c3a9
deepaktechnical007 <85184074+deepaktechnical007@users.noreply.github.com>
2023-03-26 19:14:28 +0530

    Inheritance

---
+++ Array/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+
+    void eat() {
+        System.out.println("I am eating ");
+    }
+
+}
+
+class Dog extends Animal {
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+        d.eat();
+    }
+}
