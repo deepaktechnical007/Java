a28f42eaf51f89cd9722166ab4c3a104b39a5f5a
deepaktechnical007 <85184074+deepaktechnical007@users.noreply.github.com>
2023-03-30 23:14:40 +0530

    can we overload java main() method

---
+++ Inheritance/Text1.java
@@ -0,0 +1,12 @@
+public class Text1 {
+
+    public static void main(String[] args) {
+        System.out.println("1");
+        Text1 t = new Text1();
+        t.main(10);
+    }
+
+    public static void main(int a) {
+        System.out.println("2");
+    }
+}
