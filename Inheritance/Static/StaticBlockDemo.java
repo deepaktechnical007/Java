9975cb304d3c4389db0f934c3b4c4dae507ec193
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-04-12 13:28:50 +0530

    static block executes automatically when the class is loaded in the memory

---
+++ Inheritance/Static/StaticBlockDemo.java
@@ -0,0 +1,12 @@
+package Static;
+
+public class StaticBlockDemo {
+
+    static {
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("i m in main method");
+    }
+}
