2c4bbd9fb184ec0df86299ce94c2fa80b1278ef0
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-04-19 19:46:03 +0530

    Char array is better to store password then String.

---
+++ String/StringDemo.java
@@ -0,0 +1,15 @@
+package String;
+
+public class StringDemo {
+
+    public static void main(String[] args) {
+
+        char[] a1 = new char[] { 'a', 'b', 'c' };
+
+        String a2 = new String("abc");
+
+        System.out.println("a1 :- " + a1);
+        System.out.println("a2 :- " + a2);
+
+    }
+}
