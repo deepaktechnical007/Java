45d63dc2dc0a5067e8f5d5efe8a770bddd0a6a66
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-04-20 15:18:12 +0530

    String class methods (trim())

---
+++ String/StringMethods2.java
@@ -0,0 +1,8 @@
+public class StringMethods2 {
+
+    public static void main(String[] aargs) {
+        String name = "   Babu";
+        System.out.println(name);
+        System.out.println(name.trim());
+    }
+}
