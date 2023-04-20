7d7f04b3b9aa2c8097ec2e13c3fe4206fb6682d3
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-04-20 15:15:47 +0530

    String class methods (length())

---
+++ String/StringMethods.java
@@ -0,0 +1,13 @@
+public class StringMethods {
+    public static void main(String[] args) {
+
+        String name = "Babu";
+        int i = name.length();
+        if (i == 0) {
+            System.out.println("name is empty");
+        } else {
+            System.out.println("valid name");
+        }
+
+    }
+}
