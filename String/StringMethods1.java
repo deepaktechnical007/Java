e2fd307573cece8c6f97dc26ecdfba03b56544fa
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-04-20 15:17:14 +0530

    String class methos (isEmpty())

---
+++ String/StringMethods1.java
@@ -0,0 +1,11 @@
+public class StringMethods1 {
+
+    public static void main(String[] args) {
+        String name = "Babu";
+        if (name.isEmpty()) {
+            System.out.println("name is empty");
+        } else {
+            System.out.println("valid name");
+        }
+    }
+}
