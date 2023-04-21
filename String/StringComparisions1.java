30b217a9f9dbeec7a4e5fd068b46c93d3a4c94f5
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-04-21 16:32:59 +0530

    Comparing Two Strings Using equalsIgnoreCase()

---
+++ String/StringComparisions1.java
@@ -0,0 +1,9 @@
+public class StringComparisions1 {
+
+    public static void main(String[] args) {
+
+        String s1 = "deepak";
+        String s2 = "Deepak";
+        System.out.println(s1.equalsIgnoreCase(s2));
+    }
+}
