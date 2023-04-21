7b56841d498aa3ef8044e1c74598c3e46e894b7c
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-04-21 16:58:46 +0530

    Comparing Two Strings compareToIgnoreCase() Methods

---
+++ String/StringComparisions3.java
@@ -0,0 +1,9 @@
+public class StringComparisions3 {
+
+    public static void main(String[] args) {
+
+        String s1 = "ab";
+        String s2 = "Ab";
+        System.out.println(s1.compareToIgnoreCase(s2));
+    }
+}
