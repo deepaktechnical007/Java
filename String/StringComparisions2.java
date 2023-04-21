6993868e1551dff01d73c5094abb955493cde13f
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-04-21 16:57:27 +0530

    Comparing Two Strings Using compareTo()

---
+++ String/StringComparisions2.java
@@ -0,0 +1,9 @@
+public class StringComparisions2 {
+
+    public static void main(String[] args) {
+
+        String s1 = "A";
+        String s2 = "a";
+        System.out.println(s1.compareTo(s2));
+    }
+}
