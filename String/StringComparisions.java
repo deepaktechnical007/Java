6ab10e64dbb6176e7178a6ab9f18a94ffd5d9eb1
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-04-21 16:32:13 +0530

    Comparing Two Strings Using equals()

---
+++ String/StringComparisions.java
@@ -0,0 +1,9 @@
+public class StringComparisions {
+
+    public static void main(String[] args) {
+
+        String s1 = "deepak";
+        String s2 = "amit";
+        System.out.println(s1.equals(s2));
+    }
+}
