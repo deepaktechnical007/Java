3664fc30533b3499c4ccaae3b1ea57cc966da26a
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-04-22 13:02:57 +0530

    String concat methods

---
+++ String/StringConcat.java
@@ -0,0 +1,12 @@
+import javax.print.DocFlavor.STRING;
+
+public class StringConcat {
+
+    public static void main(String[] args) {
+
+        String s1 = "Deepak";
+        String s2 = " Java";
+
+        System.out.println(s1.concat(s2));
+    }
+}
