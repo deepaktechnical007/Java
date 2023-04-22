5bf02e9a2265decfa730f8cf54843401c2704be2
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-04-22 12:57:08 +0530

    String add operation

---
+++ String/StringAdditionSubstring.java
@@ -0,0 +1,16 @@
+public class StringAdditionSubstring {
+
+    public static void main(String[] args) {
+
+        String s1 = "Deepak";
+        String s2 = " Java";
+
+        System.out.println(s1 + s2);
+        System.out.println(s1 + 10);
+        System.out.println(s1 + 10 + 20);
+        System.out.println(10 + s1 + 20);
+        System.out.println(10 + 20 + s1);
+        System.out.println(s1 + 20 / 10);
+        System.out.println(s1 + 20 * 10);
+    }
+}
