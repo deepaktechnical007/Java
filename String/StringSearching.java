edf3eaacc37fe2bdce5d5a969fb6cac44b0c2d47
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-04-24 02:15:10 +0530

    Searching Characters in Strings

---
+++ String/StringSearching.java
@@ -0,0 +1,14 @@
+public class StringSearching {
+
+    public static void main(String[] args) {
+
+        String s = "deepak";
+
+        System.out.println(s.indexOf("e"));
+        System.out.println(s.lastIndexOf('e'));
+        System.out.println(s.charAt(3));
+        System.out.println(s.contains("e"));
+        System.out.println(s.startsWith("ee"));
+        System.out.println(s.endsWith("ak"));
+    }
+}
