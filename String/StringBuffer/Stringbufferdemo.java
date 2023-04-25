ec7cef82f8276c109b3afbfb2543eda7996c93cb
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-04-25 18:59:54 +0530

    constructor in StringBuffer

---
+++ String/StringBuffer/Stringbufferdemo.java
@@ -0,0 +1,11 @@
+package Stringbufferdemo;
+
+public class Stringbufferdemo {
+
+    public static void main(String[] args) {
+
+        StringBuffer sb = new StringBuffer(1000);
+
+        System.out.println(sb.capacity());
+    }
+}
