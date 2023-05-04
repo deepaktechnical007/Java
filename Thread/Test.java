017bfb604134be60cd336d55d57fceae54eb3bdc
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-05-04 17:46:29 +0530

    Method of Thread class setName()

--- Thread/Test.java
+++ Thread/Test.java
@@ -1,13 +1,20 @@
-package mtdemo;
+package Methods;
 
 public class Test extends Thread {
-
     public void run() {
-        System.out.println("thread task");
+        System.out.println("thread task : " + Thread.currentThread().getName());
     }
 
     public static void main(String[] args) {
-        Test t = new Test();
-        t.start();
+
+        System.out.println("hello : " + Thread.currentThread().getName());
+
+        Test t1 = new Test();
+        t1.setName("Deepak");
+        t1.start();
+
+        Test t2 = new Test();
+        t2.setName("smart programming");
+        t2.start();
     }
 }
