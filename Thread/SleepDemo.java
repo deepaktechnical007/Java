a216538d32f62e484665adebc2084b08a75c8d37
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-05-05 02:17:00 +0530

    sleep() Method

---
+++ Thread/SleepDemo.java
@@ -0,0 +1,18 @@
+package Methods;
+
+public class SleepDemo {
+
+    public static void main(String[] args) {
+
+        try {
+            for (int i = 0; i <= 5; i++) {
+                Thread.sleep(1000);
+                System.out.println(i);
+            }
+        } catch (Exception e) {
+
+            System.out.println(e);
+
+        }
+    }
+}
