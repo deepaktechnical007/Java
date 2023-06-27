c49af29df34fbdc501f9260d38241ec103946326
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-06-27 23:07:01 +0530

    Add files via upload

---
+++ Thread/staticsycdemo/Test.java
@@ -0,0 +1,22 @@
+package staticsyncdemo;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Test {
+    private static final String List = null;
+
+    public static void main(String[] args) {
+
+        ArrayList al = new ArrayList();
+        al.add(10);
+        al.add("Deepak");
+        al.add('c');
+
+        HashSet hs = new HashSet();
+        hs.add("rahul");
+        hs.add(20);
+        hs.add(true);
+
+    }
+}
