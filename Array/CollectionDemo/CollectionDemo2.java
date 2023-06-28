c6163cddcd5632f63915d9a6dbf9da2c86979fb5
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-06-28 22:42:46 +0530

    Collection  isEmpty, size method

---
+++ Array/CollectionDemo/CollectionDemo2.java
@@ -0,0 +1,20 @@
+
+import java.util.ArrayList;
+
+public class CollectionDemo2 {
+
+    public static void main(String[] args) {
+
+        ArrayList al = new ArrayList();
+
+        al.add(100);
+        al.add(200);
+        al.add(300);
+
+        System.out.println(al);
+
+        System.out.println(al.isEmpty());
+        System.out.println(al.size());
+
+    }
+}
