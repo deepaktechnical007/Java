3127df502a22f169cce6ce790f2df7eb26d96feb
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-06-28 22:31:18 +0530

    Collection contains method

---
+++ Array/CollectionDemo/CollectionDemo1.java
@@ -0,0 +1,20 @@
+
+import java.util.ArrayList;
+
+public class CollectionDemo1 {
+
+    public static void main(String[] args) {
+
+        ArrayList al = new ArrayList();
+
+        al.add(100);
+        al.add(200);
+        al.add(300);
+
+        System.out.println(al);
+
+        System.out.println(al.contains(200));
+        System.out.println(al.contains(500));
+
+    }
+}
