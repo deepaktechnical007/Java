6f24e23cb6f9fb8eb6e3841bae1ac0ed5e641a3f
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-07-01 22:41:47 +0530

    Set is not an index based data structure. It store the according to the hashcode values

---
+++ Array/Difference between Set and List/SetDemo.java
@@ -0,0 +1,18 @@
+package CollectionDemo.demo;
+
+import java.util.*;
+
+public class SetDemo {
+
+    public static void main(String[] args) {
+
+        Set s = new HashSet();
+
+        s.add(100);
+        s.add(200);
+        s.add(300);
+        s.add(400);
+
+        System.out.println(s);
+    }
+}
