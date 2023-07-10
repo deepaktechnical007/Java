f58764c75dec3dd058b54010f3f762d09b771f1f
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-07-10 23:40:29 +0530

    Stack peek method

---
+++ Array/Stack/CollectionDemo1.java
@@ -0,0 +1,20 @@
+package CollectionDemo3;
+
+import java.util.Stack;
+
+public class CollectionDemo1 {
+
+    public static void main(String[] args) {
+
+        Stack s = new Stack();
+
+        s.push("deepak");
+        s.push("amit");
+        s.push(200);
+        s.push(100.10);
+
+        System.out.println(s);
+        System.out.println(s.peek());
+        System.out.println(s);
+    }
+}
