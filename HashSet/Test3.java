412d90ec5fea8c856f56d99b2dfc7aacf1cb7227
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-07-11 23:42:16 +0530

    HashSet does not follows the sorting order

---
+++ HashSet/Test3.java
@@ -0,0 +1,19 @@
+package in.sp;
+
+import java.util.HashSet;
+
+public class Test3 {
+
+    public static void main(String[] args) {
+
+        HashSet hs = new HashSet();
+
+        hs.add(10);
+        hs.add(20);
+        hs.add(30);
+        hs.add(40);
+        hs.add(50);
+
+        System.out.println(hs);
+    }
+}
