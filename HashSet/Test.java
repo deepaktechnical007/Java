6a4485094d34ddeef03263bd3db12f96a3e8cc35
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-07-11 23:30:27 +0530

    HashSet does not follow the "insertion order"

---
+++ HashSet/Test.java
@@ -0,0 +1,18 @@
+package in.sp;
+
+import java.util.HashSet;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        HashSet hs = new HashSet();
+        hs.add(10);
+        hs.add("deepak");
+        hs.add(100.34);
+        hs.add("xyz");
+        hs.add(true);
+
+        System.out.println(hs);
+    }
+}
