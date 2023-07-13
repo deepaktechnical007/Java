721b485b044d4dd3f80f06c89add5dad7147dbe9
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-07-13 23:39:04 +0530

    TreeSet follows the sorting order

---
+++ TreeSet/Test.java
@@ -0,0 +1,18 @@
+package in.sp;
+
+import java.util.TreeSet;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        TreeSet ts = new TreeSet();
+
+        ts.add(40);
+        ts.add(30);
+        ts.add(60);
+        ts.add(20);
+
+        System.out.println(ts);
+    }
+}
