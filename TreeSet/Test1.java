b80668b6069b26e9b9e306c82ba217dd4d81f1aa
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-07-14 23:30:08 +0530

    TreeSet cannot store the duplicate elements

---
+++ TreeSet/Test1.java
@@ -0,0 +1,18 @@
+package in.sp;
+
+import java.util.TreeSet;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        TreeSet ts = new TreeSet();
+
+        ts.add(40);
+        ts.add(30);
+        ts.add(60);
+        ts.add(20);
+        ts.add(30);
+        System.out.println(ts);
+    }
+}
