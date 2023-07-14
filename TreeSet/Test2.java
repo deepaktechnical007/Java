600b0cc0f32a1e7e2851ec3ad41fb699c6e876eb
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-07-14 23:34:16 +0530

    TreeSet cannot store the null values

---
+++ TreeSet/Test2.java
@@ -0,0 +1,15 @@
+package in.sp;
+
+import java.util.TreeSet;
+
+public class Test2 {
+    public static void main(String[] args) {
+
+        TreeSet ts = new TreeSet();
+
+        ts.add(null);
+
+        System.out.println(ts);
+    }
+
+}
