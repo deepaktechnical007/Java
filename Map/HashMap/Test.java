d8e771b42449ba36cfcbd791496592f982c350c0
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-07-17 23:37:49 +0530

    HashMap stores the values in key-values pair

---
+++ Map/HashMap/Test.java
@@ -0,0 +1,17 @@
+package sp.HashMap;
+
+import java.util.HashMap;
+
+public class Test {
+    public static void main(String[] args) {
+
+        HashMap hm = new HashMap();
+
+        hm.put(101, "deepak");
+        hm.put(102, "amit");
+        hm.put(103, "rahul");
+        hm.put(104, "ayush");
+
+        System.out.println(hm);
+    }
+}
