c2c17d9af1af2b726de55f087d07624fc86f3eac
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-07-19 23:30:47 +0530

    Try to add more element in HashMap , then you will get to know that HashMap does not follows the Sorting and Insertion order

---
+++ Map/HashMap/Test4.java
@@ -0,0 +1,18 @@
+package sp.HashMap;
+
+import java.util.HashMap;
+
+public class Test4 {
+    public static void main(String[] args) {
+
+        HashMap hm = new HashMap();
+
+        hm.put(101, "deepak");
+        hm.put(102, "amit");
+        hm.put(105, "ravi");
+        hm.put(103, "rahul");
+        hm.put(104, "ayush");
+
+        System.out.println(hm);
+    }
+}
