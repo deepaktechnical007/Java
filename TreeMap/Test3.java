17ab011d79f9d2cfb96ba7536089d04d102ebef8
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-08-03 22:30:15 +0530

    map.ceilingEntry() Method

---
+++ TreeMap/Test3.java
@@ -0,0 +1,22 @@
+package sp.TreeMap;
+
+import java.util.TreeMap;
+
+public class Test3 {
+
+    public static void main(String[] args) {
+
+        TreeMap map = new TreeMap();
+
+        map.put(106, "deepak");
+        map.put(103, "amit");
+        map.put(107, "kamal");
+        map.put(108, "rahul");
+        map.put(102, "rahul");
+        map.put(101, "ravi");
+
+        System.out.println(map);
+        System.out.println(map.ceilingEntry(108));
+
+    }
+}
