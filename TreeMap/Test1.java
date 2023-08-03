39941fbcf637bf2d1b604942c31f2bf93d7ded43
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2023-08-03 22:15:56 +0530

    TreeMap does not follows the Insertion order but follows the Sorting order

---
+++ TreeMap/Test1.java
@@ -0,0 +1,21 @@
+package sp.TreeMap;
+
+import java.util.TreeMap;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        TreeMap map = new TreeMap();
+
+        map.put("deepak", 105);
+        map.put("amit", 102);
+        map.put("kamal", 107);
+        map.put("rahul", 103);
+        map.put("rahul", 108);
+        map.put("ravi", 109);
+
+        System.out.println(map);
+
+    }
+}
