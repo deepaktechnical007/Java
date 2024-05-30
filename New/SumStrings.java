a8a43de7ae3efae2f70588ed304a63991d52070f
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2024-05-30 07:06:59 +0530

    How to sum two Strings

---
+++ New/SumStrings.java
@@ -0,0 +1,13 @@
+package array;
+
+public class SumStrings {
+
+    public static void main(String[] args) {
+        String s1 = "161";
+        String s2 = "242";
+        int sum = Integer.parseInt(s1) + Integer.parseInt(s2);
+        System.out.println(sum);
+        System.out.println(String.valueOf(sum));
+
+    }
+}
