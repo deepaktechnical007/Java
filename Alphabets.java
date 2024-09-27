3842c234c193dfc7eeaa7d5008a1dc2605b4440c
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2024-09-27 10:34:22 -0400

    Alphabets

---
+++ Alphabets.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Alphabets {
+    public static void main(String [] args){
+       
+        char i;
+
+        for(i =65; i<=90; i++){
+
+            System.out.print(i+",");
+        }
+        System.out.println();
+        for (i = 97; i<=122; i++){
+            System.out.print(i+",");
+        }
+    }
+}
