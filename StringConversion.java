a7234d50283332a3f74f1f19f4ec93e9374e4ef5
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2024-09-30 23:24:53 +0530

    StringConversion

---
+++ StringConversion.java
@@ -0,0 +1,9 @@
+public class StringConversion {
+
+    public static void main(String[] args) {
+
+        String name = "Deepak";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+    }
+}
