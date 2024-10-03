8fd74ebe0a9e7009577b97c3024942b03ee70387
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2024-10-03 23:56:51 +0530

    SubString

---
+++ SubSring.java
@@ -0,0 +1,10 @@
+public class SubSring {
+    public static void main(String[] args) {
+
+        String s1 = "this is demo";
+
+        System.out.println(s1.subSequence(3, 9));
+        System.out.println(s1.substring(3));
+        System.out.println(s1.substring(3, 11));
+    }
+}
