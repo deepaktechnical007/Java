836ff1fff648253e47869832e34939b174fc96bb
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2024-10-04 22:51:57 +0530

    StringJoin

---
+++ StringJoin.java
@@ -0,0 +1,10 @@
+public class StringJoin {
+
+    public static void main(String[] args) {
+
+        String s1 = "Deepak";
+        String s2 = " Java";
+
+        System.out.println(String.join("|", s1, s2, s1));
+    }
+}
